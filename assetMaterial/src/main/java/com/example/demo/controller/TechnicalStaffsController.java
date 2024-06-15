64ff24402ed8cc187fcde081144b5132788675ff
su7y <112627384+su7y@users.noreply.github.com>
2024-06-15 14:03:22 +0530

    Add files via upload

---
+++ assetMaterial/src/main/java/com/example/demo/controller/TechnicalStaffsController.java
@@ -0,0 +1,63 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.TechnicalStaffs;
+import com.example.demo.service.TechnicalStaffsService;
+
+import jakarta.validation.Valid;
+
+@CrossOrigin(origins="http://localhost:5173")
+@RestController
+@RequestMapping("/api/technicaStaffs")
+
+public class TechnicalStaffsController {
+	
+	@Autowired
+	private TechnicalStaffsService technicalStaffsService;
+
+	public TechnicalStaffsController(TechnicalStaffsService technicalStaffsService) {
+		super();
+		this.technicalStaffsService = technicalStaffsService;
+	}
+	
+	@PostMapping("/save")
+	public ResponseEntity<TechnicalStaffs> saveTechnicalStaff(@Valid @RequestBody TechnicalStaffs technicalStaffs){
+		
+		return new ResponseEntity<TechnicalStaffs>(technicalStaffsService.saveTechnicalStaff(technicalStaffs),HttpStatus.CREATED);
+	}
+	
+	@PostMapping("/login")
+	public ResponseEntity<TechnicalStaffs> loginTechnicalStaff(@RequestBody TechnicalStaffs technicalStaffs){
+		
+		TechnicalStaffs getLogin=technicalStaffsService.loginTechnicalStaff(technicalStaffs);
+		if(getLogin==null) {
+			
+			return new ResponseEntity<TechnicalStaffs>(getLogin,HttpStatus.BAD_REQUEST);
+		}
+		return new ResponseEntity<TechnicalStaffs> (technicalStaffsService.loginTechnicalStaff(technicalStaffs),HttpStatus.OK);
+	}
+	
+	@GetMapping("/getAllTechnicalStaffs")
+	public List<String> getAllTechnicalstaffs(){
+		
+		List<TechnicalStaffs> getToSetInString=	technicalStaffsService.getAllTechnicalStaffs();
+		List<String> letSet = new ArrayList<String>();
+		for(TechnicalStaffs letGet :getToSetInString ) {
+			letSet.add(letGet.getSfmId()+"--"+letGet.getSfmName()+"--"+letGet.getSfmRole());	
+		}
+		return letSet;
+	}
+
+}
