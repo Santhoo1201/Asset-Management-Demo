9c31636334fe94b620ca0fed7b7bd6ba4640c293
su7y <112627384+su7y@users.noreply.github.com>
2024-06-15 14:00:52 +0530

    Delete assetMaterial directory

--- assetMaterial/src/main/java/com/example/demo/Repository/MaterialDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.Model.MaterialDetails;
-
-public interface MaterialDetailsRepository extends JpaRepository<MaterialDetails,Long> {
-
-}
